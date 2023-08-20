993ef650b123d6da4a7b3a9bb6f54a9edd0ab0ae
Lindsay Grant <tyl3rgrant17@gmail.com>
2023-08-20 11:55:19 -0500

    <Launch.>

--- hello.js
+++ hello.js
@@ -4,7 +4,11 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log(6+6), console.log(6+6);
+console.log("While I go.");
+console.log('"While I go."');
+console.log(6+6);
+console.log(6*6);
 
 /*  
 Next, PLAY! 
